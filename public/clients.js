182496239a9dad3d9aec72af3d8fda0a032226fa
Think91 <thinkbooi@gmail.com>
2020-08-25 12:31:20 +0200

    first commit

---
+++ public/clients.js
@@ -0,0 +1,11 @@
+const clientsList = [
+  {
+    id: 1,
+    name: "Sharon G.",
+    text:
+      "Great work!",
+  },
+  
+];
+
+export default clientsList;
---
+++ public/projects.js
@@ -0,0 +1,32 @@
+const projectsList = [
+  {
+    id: 1,
+    name: "Weather App",
+    img: "./projects-img/weather-app.png",
+    text:
+      "Weather app, using React hooks and useEfect to fetch data from an API",
+    tags: ["React", "API"],
+    
+  },
+  
+  {
+    id: 2,
+    name: "To Do App",
+    img: "./projects-img/todo-app.png",
+    text:
+      "Good old/new Todo app, an example of CRUD application built with Material-UI",
+    tags: ["Next.js", "CRUD", "Material-UI"],
+    
+  },
+  {
+    id: 3,
+    name: "Menu App",
+    img: "./projects-img/recipe-app.png",
+    text:
+      "Menu app, with the ability to search for a menu list, built with Material-UI",
+    tags: ["React", "API", "Material-UI", "Search"],
+    
+  },
+  
+];
+export default projectsList;
